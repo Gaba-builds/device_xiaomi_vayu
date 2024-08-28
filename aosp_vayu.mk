35b7f2fa6b195c48ee493cddfc3cb13a96c46227
resist15 <sourav@aospa.co>
2024-08-28 12:11:56 +0530

    vayu: Enable PixelOS specific flags
    Change-Id: Ib5ebfe0a8a60a0735742081093ee92364d4c3d16

--- aosp_vayu.mk
+++ aosp_vayu.mk
@@ -14,6 +14,12 @@ $(call inherit-product, device/xiaomi/vayu/device.mk)
 # Inherit common AOSP configurations
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
+# PixelOS flags
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
 PRODUCT_NAME := aosp_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
